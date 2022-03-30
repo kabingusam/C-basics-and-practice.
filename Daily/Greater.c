2fcc6b63dfce3d279e92f247e2dafedc04e2d781
sammy <80510478+kabingusam@users.noreply.github.com>
2022-03-30 16:12:11 +0300

    Update Greater.c

--- Daily/Greater.c
+++ Daily/Greater.c
@@ -18,17 +18,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
-    if( n % 10 > 5)
-    {
-        printf("totaly greater than 5\n", n);
-    }
-    else if(n % 10 < 5)
-    {
-        printf("obviously less than 5\n", n);
-    }
-    if(n % 10 == 0)
-    {
-        printf("tottally zero bruuh!!\n", n);
-    }
+    	if( n % 10 > 5)
+    	{
+        	printf("totaly greater than 5\n", n);
+    	}
+    	else if(n % 10 < 5)
+    	{
+        	printf("obviously less than 5\n", n);
+   	}
+    	if(n % 10 == 0)
+   	{
+        	printf("tottally zero bruuh!!\n", n);
+    	}
 
 }
