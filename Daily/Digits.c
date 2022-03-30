af525b886db055dc33d4303cab6665cc1fa6c706
sammy <80510478+kabingusam@users.noreply.github.com>
2022-03-30 16:10:51 +0300

    Update Digits.c

--- Daily/Digits.c
+++ Daily/Digits.c
@@ -13,16 +13,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-    if (n < 0)
-    {
-        printf("%d is a negative number\n", n);
-    }
-    if (n > 0)
-    {
-        printf("%d is a positive number\n", n);
-    }
-    else 
-    {
-        printf("%d is zero\n", n);
-    }
+    	if (n < 0)
+    	{
+        	printf("%d is a negative number\n", n);
+    	}
+    	if (n > 0)
+    	{
+    	    	printf("%d is a positive number\n", n);
+   	 }
+    	else 
+    	{
+      	    	printf("%d is zero\n", n);
+    	}
 }
